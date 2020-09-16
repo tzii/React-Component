70805ac3fc31d5f0f2c3fd69c9303c68564dc8e1
Tuấn Vũ <tuanvundvn@gmail.com>
2020-09-17 00:43:35 +0700

    update button component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,130 +8,208 @@ export default class Buttons extends Component {
             <>
                 <h2>Buttons page</h2>
                 <div className="btn-gallary">
-                    <Button color="default">Default</Button>
-                    <Button color="primary">Default</Button>
-                    <Button color="secondary">Default</Button>
-                    <Button color="danger">Default</Button>
-                    <Button color="default" variant="outline">
-                        Default
-                    </Button>
-                    <Button color="primary" variant="outline">
-                        Default
-                    </Button>
-                    <Button color="secondary" variant="outline">
-                        Default
-                    </Button>
-                    <Button color="danger" variant="outline">
-                        Default
-                    </Button>
-                    <Button color="default" variant="text">
-                        Default
-                    </Button>
-                    <Button color="primary" variant="text">
-                        Default
-                    </Button>
-                    <Button color="secondary" variant="text">
-                        Default
-                    </Button>
-                    <Button color="danger" variant="text">
-                        Default
-                    </Button>
-                    <Button color="default" disableShadow>
-                        Default
-                    </Button>
-                    <Button color="primary" disableShadow>
-                        Default
-                    </Button>
-                    <Button color="secondary" disableShadow>
-                        Default
-                    </Button>
-                    <Button color="danger" disableShadow>
-                        Default
-                    </Button>
-                    <Button color="default" disabled>
-                        Default
-                    </Button>
-                    <Button color="primary" disabled>
-                        Default
-                    </Button>
-                    <Button color="secondary" disabled>
-                        Default
-                    </Button>
-                    <Button color="danger" disabled>
-                        Default
-                    </Button>
-                    <Button color="default" variant="text" disabled>
-                        Default
-                    </Button>
-                    <Button color="primary" variant="text" disabled>
-                        Default
-                    </Button>
-                    <Button color="secondary" variant="text" disabled>
-                        Default
-                    </Button>
-                    <Button color="danger" variant="text" disabled>
-                        Default
-                    </Button>
-                    <Button color="default" startIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="primary" startIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="secondary" startIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="danger" startIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="default" endIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="primary" endIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="secondary" endIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="danger" endIcon="local_grocery_store">
-                        Default
-                    </Button>
-                    <Button color="default" size="sm">
-                        Default
-                    </Button>
-                    <Button color="primary" size="sm">
-                        Default
-                    </Button>
-                    <Button color="secondary" size="sm">
-                        Default
-                    </Button>
-                    <Button color="danger" size="sm">
-                        Default
-                    </Button>
-                    <Button color="default" size="md">
-                        Default
-                    </Button>
-                    <Button color="primary" size="md">
-                        Default
-                    </Button>
-                    <Button color="secondary" size="md">
-                        Default
-                    </Button>
-                    <Button color="danger" size="md">
-                        Default
-                    </Button>
-                    <Button color="default" size="lg">
-                        Default
-                    </Button>
-                    <Button color="primary" size="lg">
-                        Default
-                    </Button>
-                    <Button color="secondary" size="lg">
-                        Default
-                    </Button>
-                    <Button color="danger" size="lg">
-                        Default
-                    </Button>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="default"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="default">Default</Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="primary"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="primary">Default</Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button
+                            color="secondary"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="secondary">Default</Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="danger"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="danger">Default</Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="default"
+                            variant="text"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="default" variant="text">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="primary"
+                            variant="text"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="primary" variant="text">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="secondary"
+                            variant="outline"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="secondary" variant="outline">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="danger"
+                            variant="outline"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="danger" variant="outline">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="default"
+                            disableShadow&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="default" disableShadow>
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="primary"
+                            disableShadow&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="primary" disableShadow>
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="secondary"
+                            disabled&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="secondary" disabled>
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="danger"
+                            disabled&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="danger" disabled>
+                            Default
+                        </Button>
+                    </div>
+                    <div
+                        className="content"
+                        style={{ "grid-column": "span 2" }}
+                    >
+                        <div className="code">
+                            &lt;Button color="default" variant="text"
+                            disabled&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="default" variant="text" disabled>
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color=" color="secondary" "
+                            startIcon="local_grocery_store"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button
+                            color="secondary"
+                            startIcon="local_grocery_store"
+                        >
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="danger"
+                            startIcon="local_grocery_store"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="danger" startIcon="local_grocery_store">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="default"
+                            endIcon="local_grocery_store"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="default" endIcon="local_grocery_store">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="primary"
+                            endIcon="local_grocery_store"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="primary" endIcon="local_grocery_store">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="secondary"
+                            size="sm"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="secondary" size="sm">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="danger"
+                            size="sm"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="danger" size="sm">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="default"
+                            size="md"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="default" size="md">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="default"
+                            size="md"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="primary" size="md">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="secondary"
+                            size="lg"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="secondary" size="lg">
+                            Default
+                        </Button>
+                    </div>
+                    <div className="content">
+                        <div className="code">
+                            &lt;Button color="danger"
+                            size="lg"&gt;Default&lt;/Button&gt;
+                        </div>
+                        <Button color="danger" size="lg">
+                            Default
+                        </Button>
+                    </div>
                 </div>
             </>
         );
